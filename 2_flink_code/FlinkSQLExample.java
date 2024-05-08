f89b143d9aa31f09e4509012fa4846de3b0a805b
christseng89 <samfire5200@gmail.com>
2024-05-08 18:38:31 +0800

    Initial

---
+++ 2_flink_code/FlinkSQLExample.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+public class FlinkSQLExample {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        tableEnv.executeSql(
+                "CREATE TABLE RideTest (\n" +
+                        "  `ride_id` STRING,\n" +
+                        "  `rider_id` STRING,\n" +
+                        "  `driver_id` STRING,\n" +
+                        "  `location_id` STRING,\n" +
+                        "  `amount` FLOAT,\n" +
+                        "  `ride_status` STRING,\n" +
+                        "  `start_lat` FLOAT,\n" +
+                        "  `start_lng` FLOAT,\n" +
+                        "  `dest_lat` FLOAT,\n" +
+                        "  `dest_lng` FLOAT,\n" +
+                        "  `request_time` TIMESTAMP(3) METADATA FROM 'timestamp',\n" +
+                        "  `processing_time` as PROCTIME()\n" +
+                        ") WITH (\n" +
+                        "  'connector' = 'kafka',\n" +
+                        "  'topic' = 'rides',\n" +
+                        "  'properties.bootstrap.servers' = '[::1]:9092',\n" +
+                        "  'properties.group.id' = 'test',\n" +
+                        "  'scan.startup.mode' = 'latest-offset',\n" +
+                        "  'format' = 'json'\n" +
+                        ");"
+        );
+
+        Table outputTable = tableEnv.sqlQuery("select ride_id,amount,ride_status,lower(ride_status) from RideTest");
+        DataStream<Row> outputStream = tableEnv.toDataStream(outputTable);
+        outputStream.print();
+        env.execute();
+
+
+    }
+}

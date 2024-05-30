b2f228cfe8cb80f027b966d5d99069fe0d6d8fac
Chris Tseng <samfire5200@gmail.com>
2024-05-30 17:36:03 +0800

    71. How to use SubDAGs?

--- Airflow/docker/dags/group_dag.py
+++ Airflow/docker/dags/group_dag.py
@@ -1,26 +1,24 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+from subdags.subdag_downloads import subdag_downloads
  
 from datetime import datetime
  
-with DAG('group_dag', start_date=datetime(2022, 1, 1), 
+with DAG('group_dag', start_date=datetime(2023, 1, 1), 
     schedule_interval='@daily', catchup=False) as dag:
  
-    download_a = BashOperator(
-        task_id='download_a',
-        bash_command='sleep 10'
+    args = {
+        'start_date': dag.start_date,
+        'schedule_interval': dag.schedule_interval,
+        'catchup': dag.catchup
+    }
+    
+    downloads = SubDagOperator(
+        task_id='downloads',
+        subdag=subdag_downloads(dag.dag_id, 'downloads', args)
     )
- 
-    download_b = BashOperator(
-        task_id='download_b',
-        bash_command='sleep 10'
-    )
- 
-    download_c = BashOperator(
-        task_id='download_c',
-        bash_command='sleep 10'
-    )
- 
+    
     check_files = BashOperator(
         task_id='check_files',
         bash_command='sleep 10'
@@ -41,5 +39,5 @@ with DAG('group_dag', start_date=datetime(2022, 1, 1),
         bash_command='sleep 10'
     )
  
-    [download_a, download_b, download_c] >> check_files >> [transform_a, transform_b, transform_c]
+    downloads >> check_files >> [transform_a, transform_b, transform_c]
     
